a9ec0753f2596b344228083f5f39acb5825a30c1
kosugi <temparamental@hotmail.com>
2022-01-23 18:41:46 +0900

    first commit.

---
+++ ConsoleAppSelfHostWebApi/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleAppSelfHostWebApi
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseConfiguration(config)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                //.UseIISIntegration() //// Here IIS integration is optional
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
